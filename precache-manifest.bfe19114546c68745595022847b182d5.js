cfa7a1c63cd7bb40ba1dbbc9bc021cd637e3d120
AllanChain <AllanChain@users.noreply.github.com>
2020-04-28 13:33:05 +0000

    deploy: 133b67516690b36685530a1e47e6c68eef10cdbf

---
+++ precache-manifest.bfe19114546c68745595022847b182d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af7c9559d961edca7a757fad6d3d0b7d",
+    "url": "/PKUHoleCommunity/index.html"
+  },
+  {
+    "revision": "7d4289c6cc6b04add757",
+    "url": "/PKUHoleCommunity/static/css/main.d38a9530.chunk.css"
+  },
+  {
+    "revision": "536814c1e3717e6ddd82",
+    "url": "/PKUHoleCommunity/static/js/2.00494b3c.chunk.js"
+  },
+  {
+    "revision": "7d4289c6cc6b04add757",
+    "url": "/PKUHoleCommunity/static/js/main.ca1d6a65.chunk.js"
+  },
+  {
+    "revision": "79acedf42c9c7236c13b",
+    "url": "/PKUHoleCommunity/static/js/runtime~main.6299b00b.js"
+  }
+]);
