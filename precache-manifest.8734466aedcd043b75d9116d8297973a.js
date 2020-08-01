d770baf68bcf734786c958375363a0750d5a588e
AllanChain <AllanChain@users.noreply.github.com>
2020-08-01 09:47:43 +0000

    deploy: fd45b5d8027f0088330700ce63b9de855ad68ae1

--- precache-manifest.8734466aedcd043b75d9116d8297973a.js
+++ precache-manifest.8734466aedcd043b75d9116d8297973a.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "74dba60d22107d7bad7947824af22bbd",
+    "revision": "3e19318d11c2de708e39ed326eac4526",
     "url": "/PKUHoleCommunity/index.html"
   },
   {
-    "revision": "3b328476532fb51c83dd",
+    "revision": "9d29cf8fe454a1b6abf8",
     "url": "/PKUHoleCommunity/static/css/main.9b72770e.chunk.css"
   },
   {
@@ -20,12 +20,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/PKUHoleCommunity/static/js/2.5a889ce5.chunk.js.map"
   },
   {
-    "revision": "3b328476532fb51c83dd",
-    "url": "/PKUHoleCommunity/static/js/main.66d38599.chunk.js"
+    "revision": "9d29cf8fe454a1b6abf8",
+    "url": "/PKUHoleCommunity/static/js/main.4510e6d7.chunk.js"
   },
   {
-    "revision": "3b328476532fb51c83dd",
-    "url": "/PKUHoleCommunity/static/js/main.66d38599.chunk.js.map"
+    "revision": "9d29cf8fe454a1b6abf8",
+    "url": "/PKUHoleCommunity/static/js/main.4510e6d7.chunk.js.map"
   },
   {
     "revision": "504d7c986179a1b7493d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/PKUHoleCommunity/precache-manifest.45f9ce97312e3d7ea5d0f78be10ec0eb.js"
+  "/PKUHoleCommunity/precache-manifest.8734466aedcd043b75d9116d8297973a.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "PKUHoleCE"});
