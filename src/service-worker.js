4d3d55f7b4dc6f0dd017966657fd705fe23a6470
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2021-07-08 17:22:35 +0800

    fix: use react-scripts 4 inject manifest sw
    ... instead of patching config

---
+++ src/service-worker.js
@@ -0,0 +1,31 @@
+import { registerRoute } from 'workbox-routing';
+import { precacheAndRoute } from 'workbox-precaching';
+import { setCacheNameDetails, clientsClaim } from 'workbox-core';
+import { CacheFirst } from 'workbox-strategies';
+import { ExpirationPlugin } from 'workbox-expiration';
+
+setCacheNameDetails({ prefix: 'PKUHoleCE' });
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+clientsClaim();
+
+precacheAndRoute(self.__WB_MANIFEST, {});
+
+registerRoute(
+  /static\/(manifest.json|(fonts_7|bg)\/.*)/,
+  new CacheFirst({
+    cacheName: 'PKUHoleCE',
+    plugins: [
+      new ExpirationPlugin({
+        maxAgeSeconds: 1296000,
+        purgeOnQuotaError: false,
+      }),
+    ],
+  }),
+  'GET',
+);
